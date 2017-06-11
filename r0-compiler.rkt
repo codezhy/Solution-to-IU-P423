d42680a4f9f0278eca251d17614de3abbd7227b1
wangwangwar <wangwangwar@gmail.com>
2017-06-11 14:03:11 +0800

    Exercise 5 - Add "select instructions" Pass

--- r0-compiler.rkt
+++ r0-compiler.rkt
@@ -4,9 +4,9 @@
 (require "alist.rkt")
 
 ;; This exports r0-passes, defined below, to users of this file.
-(provide r0-passes)
+(provide r0-passes r0-select-instructions-pass)
 
-; R0 -> R0
+; uniquify: R0 -> R0
 (define (uniquify alist)
   (lambda (ast)
     (match ast
@@ -26,7 +26,7 @@
             `(,op ,@(map (uniquify alist) es))]
            )))
 
-; R0 -> C0
+; flatten: R0 -> C0
 (define flatten
   (lambda (ast)
     (match ast
@@ -40,25 +40,25 @@
                       (append e-tmp-vars (list x) body-tmp-vars)))]
            [`(program ,e)
             (let*-values ([(exp assigns tmp-vars) (flatten e)]
-                         [(return) `(return ,exp)])
-              (values (append (list 'program) (list tmp-vars) assigns (list return))
-                      (append assigns (list return))
-                      tmp-vars))]
+                          [(return) `(return ,exp)])
+                         (values (append (list 'program) (list tmp-vars) assigns (list return))
+                                 (append assigns (list return))
+                                 tmp-vars))]
            [`(+ ,e1 ,e2)
             (let*-values ([(e1-exp e1-assigns e1-tmp-vars) (flatten e1)]
-                         [(e2-exp e2-assigns e2-tmp-vars) (flatten e2)]
-                         [(new-tmp-var) (gensym 'tmp.)]
-                         [(new-assign) `(assign ,new-tmp-var (+ ,e1-exp ,e2-exp))])
-              (values new-tmp-var
-                      (append e1-assigns e2-assigns (list new-assign))
-                      (append e1-tmp-vars e2-tmp-vars (list new-tmp-var))))]
+                          [(e2-exp e2-assigns e2-tmp-vars) (flatten e2)]
+                          [(new-tmp-var) (gensym 'tmp.)]
+                          [(new-assign) `(assign ,new-tmp-var (+ ,e1-exp ,e2-exp))])
+                         (values new-tmp-var
+                                 (append e1-assigns e2-assigns (list new-assign))
+                                 (append e1-tmp-vars e2-tmp-vars (list new-tmp-var))))]
            [`(- ,e)
             (let*-values ([(exp assigns tmp-vars) (flatten e)]
-                         [(new-tmp-var) (gensym 'tmp.)]
-                         [(new-assign) `(assign ,new-tmp-var (- ,exp))])
-              (values new-tmp-var
-                      (append assigns (list new-assign))
-                      (append tmp-vars (list new-tmp-var))))]
+                          [(new-tmp-var) (gensym 'tmp.)]
+                          [(new-assign) `(assign ,new-tmp-var (- ,exp))])
+                         (values new-tmp-var
+                                 (append assigns (list new-assign))
+                                 (append tmp-vars (list new-tmp-var))))]
            [`(read) (values `(read) '() '())]
            )))
 
@@ -67,11 +67,62 @@
     (define-values (exps assigns tmp-vars) (flatten ast))
     exps))
 
+; select-instructions: C0 -> X86
+(define select-instructions
+  (lambda (ast)
+    (match ast
+           [(? symbol?) `((var ,ast))]
+           [(? integer?) `((int ,ast))]
+           [`(assign ,x (+ (read) (read)))
+            `((callq read_int) 
+              (movq (reg rax) (var ,x)) 
+              (callq read_int) 
+              (addq (reg rax) (var ,x)))]
+           [`(assign ,x (+ ,e1 (read)))
+             `((movq ,@(select-instructions e1) (var ,x))
+               (callq read_int)
+               (addq (reg rax) (var ,x)))]
+           [`(assign ,x (+ (read) ,e2))
+             `((callq read_int)
+               (movq (reg rax) (var ,x))
+               (addq ,@(select-instructions e2) (var ,x)))]
+           [`(assign ,x (+ ,e1 ,e2))
+            (cond
+             [(eq? x e1) `(addq ,(select-instructions e2) (var ,x))]
+             [(eq? x e2) `(addq ,(select-instructions e1) (var ,x))]
+             [else `((movq ,@(select-instructions e1) (var ,x)) (addq ,@(select-instructions e2) (var ,x)))])]
+           [`(assign ,x (- (read)))
+             `((callq read_int) (movq (reg rax) (var ,x)) (negq (var ,x)))]
+           [`(assign ,x (- ,e))
+             `((movq ,@(select-instructions e) (var ,x)) (negq (var ,x)))]
+           [`(assign ,x (read))
+             `((callq read_int) (movq (reg rax) (var ,x)))]
+           [`(assign ,x ,e)
+             `((movq ,@(select-instructions e) (var ,x)))]
+           [`(return (read))
+             `((callq read_int))]
+           [`(return ,e)
+             `((movq ,@(select-instructions e) (reg rax)))]
+           [`(program ,args ,es ...)
+             (let ([ins (flatten-list-1 (map select-instructions es))])
+                 `(program ,args ,@ins))]
+    )))
+
+(define flatten-list-1
+  (lambda (lst)
+    (cond [(null? lst) '()]
+          [(list? (car lst)) (append (car lst) (flatten-list-1 (cdr lst)))]
+          [else (cons (car lst) (flatten-list-1 (cdr lst)))])))
+
 ;; Define the passes to be used by interp-tests and the grader
 ;; Note that your compiler file (or whatever file provides your passes)
 ;; should be named "compiler.rkt"
 (define r0-passes
   `( ("uniquify" ,(uniquify (alist-init)) ,interp-scheme)
-     ("flatten" ,flatten2 ,interp-C)
+    ("flatten" ,flatten2 ,interp-C)
+    ("select instructions" ,select-instructions ,interp-x86)
     ))
 
+(define r0-select-instructions-pass
+  `( ("select instructions" ,select-instructions ,interp-x86)
+  ))
--- run-tests.rkt
+++ run-tests.rkt
@@ -8,5 +8,6 @@
 (debug-level 1)
 
 ;(interp-tests "R0: uniquify pass" #f r0-passes interp-scheme "r1" (range 1 22))
-(interp-tests "R0: flatten pass" #f r0-passes interp-scheme "r1a" (range 1 12))
+;(interp-tests "R0: flatten pass" #f r0-passes interp-scheme "r1a" (range 1 12))
+(interp-tests "R0: select instructions pass" #f r0-passes interp-scheme "r1" (range 1 22))
 (display "tests passed!") (newline)
