e6403c50a7fb05eb362424bde62348389b8e7998
wangwangwar <wangwangwar@gmail.com>
2017-06-23 01:25:55 +0800

    Fix the Pass "uncover-live"
    Live-after sets must be computed from instructions from back to front.

--- r0-reg-compiler.rkt
+++ r0-reg-compiler.rkt
@@ -16,29 +16,25 @@
          (inherit flatten-list-1)
          
          ; pass uncover-live : x86* -> x86*
+         ; Compute from instructions back to front
          (define/public (uncover-live)
            (lambda (ast)
              (match ast
                     [`(program ,args ,es ...)
-                     (let ([live-afters (_uncover-live es '())])
+                     (let ([live-afters (_uncover-live (reverse es) (list (set)))])
                        `(program ,(cons args live-afters) ,@es))]
                     )))
          
          (define _uncover-live
            (lambda (instruction-lst live-afters)
-             (cond [(null? instruction-lst) live-afters]
-                   [(null? live-afters)
-                    (_uncover-live (cdr instruction-lst)
-                                   (list (set-subtract (set-union (set)
-                                                                  (_compute-write-variables (car instruction-lst)))
-                                                       (_compute-read-variables (car instruction-lst)))))]
-                   [else
-                    (_uncover-live (cdr instruction-lst)
-                                   (append live-afters
-                                           (list (set-subtract (set-union (last live-afters)
-                                                                          (_compute-write-variables (car instruction-lst)))
-                                                               (_compute-read-variables (car instruction-lst))))))]
-                   )))
+               (cond [(null? instruction-lst) (cdr (reverse live-afters))] ; drop the unexists first live-after set
+                     [else
+                      (_uncover-live (cdr instruction-lst)
+                                     (append live-afters
+                                             (list (set-union (set-subtract (last live-afters)
+                                                                            (_write-variables (car instruction-lst)))
+                                                              (_read-variables (car instruction-lst))))))]
+                     )))
          
          (define _variables
            (lambda (instruction)
@@ -46,18 +42,25 @@
                     [`(,op (var ,e1) ,e2) (set e1)]
                     [`(,op ,e1 (var ,e2)) (set e2)]
                     [`(,op (var ,e1) (var ,e2)) (set e1 e2)]
+                    [`(,op (var ,e)) (set e)]
                     [else (set)])))
          
-         (define _compute-read-variables
+         (define _read-variables
            (lambda (instruction)
              (match instruction
-                    [`(,op (var ,e1) ,e2) (set e1)]
+                    [`(var ,x) (set x)]
+                    [`(movq ,e1 ,e2) (_read-variables e1)]
+                    [`(addq ,e1 ,e2) (set-union (_read-variables e1) (_read-variables e2))]
+                    [`(neq (var ,e1)) (set e1)]
                     [else (set)])))
          
-         (define _compute-write-variables
+         (define _write-variables
            (lambda (instruction)
              (match instruction
-                    [`(,op ,e1 (var ,e2)) (set e2)]
+                    [`(var ,x) (set x)]
+                    [`(movq ,e1 ,e2) (_write-variables e2)]
+                    [`(addq ,e1 ,e2) (_write-variables e2)]
+                    [`(neq (var ,e1)) (set e1)]
                     [else (set)])))
          
          ; pass build-interference : x86* -> x86*
@@ -79,14 +82,14 @@
                        (let* ([live-after-set (car live-afters)]
                               [vertices (set-subtract live-after-set (_variables `(movq ,s ,d)))]
                               [edges (flatten-list-1 (set-map vertices
-                                                              (lambda (v) (set-map (_compute-write-variables `(movq ,s ,d))
+                                                              (lambda (v) (set-map (_write-variables `(movq ,s ,d))
                                                                                    (lambda (d) (cons d v))))))])
                          (_helper (cdr instructions) (cdr live-afters) (add-edges graph edges)))]
                       [`(addq ,s ,d)
                        (let* ([live-after-set (car live-afters)]
-                              [vertices (set-subtract live-after-set (_compute-write-variables `(addq ,s ,d)))]
+                              [vertices (set-subtract live-after-set (_write-variables `(addq ,s ,d)))]
                               [edges (flatten-list-1 (set-map vertices
-                                                              (lambda (v) (set-map (_compute-write-variables `(movq ,s ,d))
+                                                              (lambda (v) (set-map (_write-variables `(addq ,s ,d))
                                                                                    (lambda (d) (cons d v))))))])
                          (_helper (cdr instructions) (cdr live-afters) (add-edges graph edges)))]
                       [`(callq ,label)
@@ -109,28 +112,28 @@
                        [else (cons (caar lst) (_var-reg-list->var-list (cdr lst)))])))
              (match ast
                     [`(program (,args ,graph) ,es ...)
-                      (let* ([var-color-list ((color-graph) graph args)]
-                             [var-register-list (map (lambda (v) 
-                                                       (cons (car v) 
-                                                             (color->register (cdr v)))) 
-                                                     var-color-list)]
-                             [reg-var-register-list (filter (lambda (v) (cdr v)) var-register-list)]
-                             [reg-var-list (_var-reg-list->var-list reg-var-register-list)]
-                             [stack-args (remove* reg-var-list args)])
-                          `(program ,stack-args ,@(map (allocate-registers reg-var-register-list) es))
+                     (let* ([var-color-list ((color-graph) graph args)]
+                            [var-register-list (map (lambda (v)
+                                                      (cons (car v)
+                                                            (color->register (cdr v))))
+                                                    var-color-list)]
+                            [reg-var-register-list (filter (lambda (v) (cdr v)) var-register-list)]
+                            [reg-var-list (_var-reg-list->var-list reg-var-register-list)]
+                            [stack-args (remove* reg-var-list args)])
+                       `(program ,stack-args ,@(map (allocate-registers reg-var-register-list) es))
                        )]
                     [(? symbol?) ast]
                     [(? integer?) ast]
                     [`(var ,x)
                      (let* ([result (findf (lambda (v) (eq? (car v) x)) var-reg-list)])
                        (if result
-                         `(reg ,(cdr result))
+                           `(reg ,(cdr result))
                          `(var ,x)
                          ))]
-                    [`(,op ,es ...) 
-                      `(,op ,@(map (allocate-registers var-reg-list) es))]
+                    [`(,op ,es ...)
+                     `(,op ,@(map (allocate-registers var-reg-list) es))]
                     [else ast]
-             )))
+                    )))
          
          ; Algorithm: DSATUR
          ; Input: a graph G
@@ -153,17 +156,17 @@
                (lambda (graph var-color-info-hash var-list)
                  (if (null? var-list)
                      ; Convert to a mapping of all variables to their colors
-                     (hash-map var-color-info-hash (lambda (var info) (cons var (car info))))
-                     (begin
-                      (let* ([current-var (highest-saturation-var var-color-info-hash var-list)]
-                             [current-color (_new-color (saturation var-color-info-hash current-var))])
-                            (set-color! var-color-info-hash current-var current-color)
-                            (let ([adjacent-set (adjacent graph current-var)])
-                              (if adjacent-set
-                                (for/set ([adj (adjacent graph current-var)])
-                                  (add-saturation-color! var-color-info-hash adj current-color))
-                                #f))
-                            (inner graph var-color-info-hash (set-remove var-list current-var)))))))
+                   (hash-map var-color-info-hash (lambda (var info) (cons var (car info))))
+                   (begin
+                    (let* ([current-var (highest-saturation-var var-color-info-hash var-list)]
+                           [current-color (_new-color (saturation var-color-info-hash current-var))])
+                      (set-color! var-color-info-hash current-var current-color)
+                      (let ([adjacent-set (adjacent graph current-var)])
+                        (if adjacent-set
+                            (for/set ([adj (adjacent graph current-var)])
+                                     (add-saturation-color! var-color-info-hash adj current-color))
+                          #f))
+                      (inner graph var-color-info-hash (set-remove var-list current-var)))))))
              (inner graph (make-color-info-hash (hash-keys graph)) var-list)
              ))
          
@@ -201,6 +204,6 @@
       ("build interference" ,(send compiler build-interference) ,interp-x86)
       ("allocate registers" ,(send compiler allocate-registers '()) ,interp-x86)
       ("assign homes" ,(send compiler assign-homes (dict-init)) ,interp-x86)
-      ;("patch instructions" ,(send compiler patch-instructions) ,interp-x86)
-      ;("print x86" ,(send compiler print-x86) #f)
+      ("patch instructions" ,(send compiler patch-instructions) ,interp-x86)
+      ("print x86" ,(send compiler print-x86) #f)
       )))
